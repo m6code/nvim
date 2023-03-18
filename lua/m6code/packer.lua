e99d8591aaf8d557b0186a47b40a45ebbb01d83d
Benjamin SA <benjamynfrank101@gmail.com>
2023-03-18 17:16:00 +0100

    fixes

--- lua/m6code/packer.lua
+++ lua/m6code/packer.lua
@@ -1,7 +1,7 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
--- vim.cmd [[packadd packer.nvim]]
+vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	-- Packer can manage itself
@@ -22,6 +22,7 @@ return require('packer').startup(function(use)
 			-- vim.cmd('colorscheme rose-pine') -- for default theme
 			-- vim.cmd('colorscheme rose-pine-moon') -- for moon theme
 			-- vim.cmd('colorscheme rose-pine-dawn') -- for dawn theme 
+            vim.cmd('colorscheme onedark')
 
 		end
 	})
