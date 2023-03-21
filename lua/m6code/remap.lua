db8cc1ef79be4f773d95f60433d28103f12a1429
Benjamin SA <benjamynfrank101@gmail.com>
2023-03-21 09:27:19 +0100

    Remap++

--- lua/m6code/remap.lua
+++ lua/m6code/remap.lua
@@ -37,9 +37,9 @@ end)
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]]) -- Copy to system clipboard
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
+vim.keymap.set({"n", "v"}, "<leader>p", [["+p]]) -- Paste from system clipboard 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
