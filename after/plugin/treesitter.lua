9d62e1b2bcc02ed704860095a74ed6b66d491efd
Benjamin SA <benjamynfrank101@gmail.com>
2023-05-21 19:21:32 +0100

    fix help error

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,13 +1,16 @@
 -- Make sure to use clang for compilation on windows
 --
 -- require('nvim-treesitter.install').compilers = { "clang" }
+-- if vim.loop.os_uname().sysname == "Windows_NT" then
+--   require('nvim-treesitter.install').compilers = { "clang" }
+-- end
  
 
 
 require'nvim-treesitter.configs'.setup {
 
  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "help", "javascript", "typescript", "lua", "help", "php", "java", "json", "css", "markdown", "markdown_inline", "python", "tsx"  },
+  ensure_installed = { "lua", "javascript", "typescript", "php", "java", "json", "css", "markdown", "markdown_inline", "python", "tsx" },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
@@ -17,7 +20,7 @@ require'nvim-treesitter.configs'.setup {
 
  -- List of parsers to ignore installing (for "all")
   ignore_install = { "vue", "html" },
-
+  
   highlight = {
     enable = true,
 
